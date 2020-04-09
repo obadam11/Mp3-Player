77efadb438f7e4bdab381d612333afdd0d8d413e
obadam11 <obada.mik.ab@gmail.com>
2020-04-10 00:23:58 +0300

    added the button method and made thePlay, Pause, and Exit button objects.

--- music_player.py
+++ music_player.py
@@ -1,10 +1,15 @@
 import pygame
-import os
+import sys
 
 pygame.init()
 
 screen_height = 400
-screen_width = 400
+screen_width = 600
+
+lightblue = (27, 238, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Music Player")
@@ -21,6 +26,50 @@ class Music_Player(object):
         pygame.mixer.music.load(self.file_name)
         pygame.mixer.music.play(0)
 
+    def color_screen(self, color):
+        self.display.fill(color)
+
+    def button(self, xb, yb, radius, txt, colorb, colort, xt, yt):
+        # Drawing the rectangle (button)
+        pygame.draw.circle(self.display, colorb, (xb, yb), radius)
+
+        # Writing in the button
+
+        on_screen = str(txt)
+        font = pygame.font.Font(None, 18)
+        text = font.render(str(on_screen), 1, colort)
+        self.display.blit(text, (xt,yt))
+
+
+        # Making the button functional
+        xm, ym = pygame.mouse.get_pos()
+
+        # print(f"x = {xm}")
+        # print(f"y = {ym}")
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                if txt == "Play":
+                    if (250 <= xm <= 350) and (50 <= ym <= 145):
+                        print("Play Button is clicked")
+
+                if txt == "Pause":
+                    if (398 <= xm <= 500) and (50 <= ym <= 145):
+                        print("Pause button is clicked")
+
+                if txt == "Exit":
+                    if (250 <= xm <= 350) and (200 <= ym <= 300):
+                        print("Exit button is clicked")
+                        pygame.quit()
+                        sys.exit()
+
+
+
+
+
+
 
 
 
@@ -31,7 +80,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            pygame.quit()
+            sys.exit()
 
+    p.color_screen(lightblue)
+
+    p.button(xb=300, yb=100, radius=50, txt="Play",
+            colorb=red, colort=white, xt=290, yt=95)
+
+    p.button(xb=450, yb=100, radius=50, txt="Pause",
+             colorb=red, colort=white, xt=430, yt=95)
+
+    p.button(xb=300, yb=250, radius=50, txt="Exit",
+             colorb=red, colort=white, xt=290, yt=240)
 
 
 
